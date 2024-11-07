51e0955759db9501353ea7ba9181022406142bbd
Garikipati.Naga Amrutha <amruthagarikipati87@gmail.com>
2024-11-07 13:24:30 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,12 @@
+class Emp
+{
+    public static void main(String args[])
+    {
+      int eno=101;
+      String ename="Karun";
+      float esal=544.45F;
+      System.out.println("The Given Employee Number is:"+eno);
+      System.out.println("The Given Employee Name is:"+ename);
+      System.out.println("The Given Employee Salary is:"+esal);
+    }
+}
